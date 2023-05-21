6bec689b26082abaa811ff1ef6b70c28ee7dc2b5
Saurabh <saurabhsalunke91@gmail.com>
2023-05-21 17:06:00 +0530

    Expense tracker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import Card from './Components/Card'
+import ExpensesFilter from './Components/ExpensesFilter';
+import ExpenseItem from './Components/ExpenseItem';
+import NewExpense from './Components/NewExpense';
+import { useState } from 'react';
+export default function App() {
+  let o;
+  if(localStorage.getItem("expense")===null){
+    o=[]
+  }
+  else{
+    o=JSON.parse(localStorage.getItem("expense"))
+  }
+  const [init,setInit]=useState(o)
+  const addExpense=(o)=>{
+    let t=[...init,...o]
+    setInit(t);
+    localStorage.setItem("expense",JSON.stringify(t))
+    console.log(init)
+    console.log(o)
+    
+  }
+  const [year,setYear]=useState("2019")
+const yearHandler=(val)=>{
+  setYear(val)
+  console.log(val)
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div className="new-expense">
+     <NewExpense addExpense={addExpense}/>
+     </div>
+    <Card className="App">
+    <div className='expenses'>
+      <ExpensesFilter onSelect={year} onYearSelect={yearHandler}/>
+      </div>
+      {
+        init.map((m)=>{
+          return <ExpenseItem {...m}></ExpenseItem>
+        })
+      }
+        
+    </Card>
+     </>
   );
 }
 
-export default App;
